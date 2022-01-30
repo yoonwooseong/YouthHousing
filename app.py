5b006bc96415ac3d23af21576f2bba566f76fed3
Wooseong <57824259+yoonwooseong@users.noreply.github.com>
2022-01-30 15:53:51 +0900

    구조 설계

--- app.py
+++ app.py
@@ -8,12 +8,16 @@ app = Flask("SuperScrapper")
 def home():
     return render_template("home.html")
 
-@app.route("/crawl")
+@app.route("/crawl") # 크롤링 후 데이터 입력 (하기 전에 이전 DB의 마지막 값 불러와서 변경 감지)
 def scrape():
     param = {}
-    
     crawlData = crawling(param)
     saveData(crawlData)
-    return render_template("scrapepage.html")
+    return render_template("end.html")
+
+@app.route("/apply") # 버튼 클릭 시 -> 카카오 로그인 -> redirect로 complete 페이지에서 code 가져오고 DB에 새롭게 데이터가 추가되면 알림 전송
+def scrape():
+    return render_template("complete.html")
+
 
 app.run(host="localhost")
--- getKakaoToken.py
+++ getKakaoToken.py
@@ -5,3 +5,4 @@ redirect_uri = 'http://youth2030.co.kr/user/board/mn010203.do'
 url = 'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id='+rest_api_key+'&redirect_uri='+redirect_uri
 
 response = requests.get(url)
+print(response)
