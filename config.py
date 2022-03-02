306fc04367d1417601e3b69cbf0dd3a392451e06
Wooseong <57824259+yoonwooseong@users.noreply.github.com>
2022-03-03 08:03:27 +0900

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,6 @@ import pymysql
 
 # DB 설정
 conn = pymysql.connect(host = 'ip', user = 'root', password = '1111', db = '', charset = 'utf8mb4', use_unicode=True)
+
+# URL 설정
+URL_BASE = "http://youth2030.co.kr/user/board/mn010203.do"
