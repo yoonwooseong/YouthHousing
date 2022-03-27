281f4fb92d4d68b6f4dc85992ad84e70a7310714
yoonwooseong <57824259+yoonwooseong@users.noreply.github.com>
2022-03-27 15:11:20 +0900

    send message 메서드화

--- mail.py
+++ mail.py
@@ -2,14 +2,28 @@ import smtplib
 from email.mime.text import MIMEText
 from mailAccount import EMAIL, PASSWORD
 
-smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-# smtp.ehlo()
-# smtp.starttls()
-smtp.login(EMAIL, PASSWORD)
 
-msg = MIMEText('테스트')
-msg['Subject'] = '테스트'
-msg['To'] = 'dntjd851@naver.com'
-smtp.sendmail(EMAIL, 'dntjd851@naver.com', msg.as_string())
+def checkEmail():
+    isRegister = True
 
-smtp.quit()
+    # 등록 여부 체크
+
+    if isRegister:
+        sendEmail()
+    else:
+        print('not register')
+
+
+def sendEmail():
+    smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    # smtp.ehlo()
+    # smtp.starttls()
+    smtp.login(EMAIL, PASSWORD)
+
+    msg = MIMEText('테스트')
+    msg['Subject'] = '테스트'
+    msg['To'] = 'dntjd851@naver.com'
+    smtp.sendmail(EMAIL, 'dntjd851@naver.com', msg.as_string())
+
+    smtp.quit()
+    return True
