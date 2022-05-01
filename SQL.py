3cc7010bb392ceb94572db8f524cde9214a04e1f
Wooseong <57824259+yoonwooseong@users.noreply.github.com>
2022-05-02 08:29:15 +0900

    Update SQL.py

--- SQL.py
+++ SQL.py
@@ -11,3 +11,22 @@ def sample(param):
     val = (param)
     db.execute(sql, val)
     conn.commit()
+
+def selectAllNotice(param):
+    sql = 'select * from notice'
+    db.execute(sql)
+    notice = db.fetchall()
+    return notice
+
+def selectNotice(param):
+    sql = 'select * from notice LIMIT 1'
+    db.execute(sql)
+    notice = db.fetchall()
+    return notice
+
+def insertNotice(param):
+    sql = 'insert into notice (idx, index, type, title, postDate, applyDate, entrepreneur) VALUES (0, %s, %s, %s, %s, %s, %s)'
+    val = (param)
+    db.execute(sql, val)
+    conn.commit()
+
