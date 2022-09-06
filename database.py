fceab3687e4bb6c8549a88c5c6f6568d475466dc
Wooseong <57824259+yoonwooseong@users.noreply.github.com>
2022-09-06 16:25:14 +0900

    개발 DB 연동 및 이전 공고문 개수 조회

---
+++ database.py
@@ -0,0 +1,27 @@
+from pdb import post_mortem
+from config import *
+from pymongo import MongoClient
+from pymongo.cursor import CursorType
+
+global db
+
+def connectDataBase():
+    global db
+    mongoClient = MongoClient(host=MONGODB_HOST, port=MONGODB_PORT)
+    db = mongoClient['youth_housing']
+
+def updateNoticeSlim(count):
+    global db
+    db.notice_slim.update_one({"_id":1}, {"$set":{"count":count}})
+
+def getNumberOfSavedNotice(isSimple):
+    global db
+    if isSimple:
+        result = db.notice_slim.find_one({"_id":1})
+        return int(result['count']) 
+    else:
+        return
+
+def updateNotice():
+    # 공고 전체 내용 저장
+    return
